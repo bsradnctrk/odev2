585d499ecaf0a138e8af0f38310f49a982fd14d2
Busra.D <Busra.D@Busra>
2022-09-28 20:03:18 +0300

    two

---
+++ src/methods/MainReCap.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class MainReCap {
+    public static void main(String[] args) {
+
+
+        Dortislem dortislem =new Dortislem();
+
+        int sonuc = dortislem.topla(3,4);
+        System.out.println(sonuc);
+    }
+}
